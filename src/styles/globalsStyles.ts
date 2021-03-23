f6e0832c5e321f348362b4a07edfea2bf56292c8
edmilson-dk <droidvidaboa@gmail.com>
2021-03-23 15:07:09 -0300

    feat: created initials styles

---
+++ src/styles/globalsStyles.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --green: #00d862;
+    --dark: #000f13;
+    --dark-light: #03181d;
+    --gray: #485a5f;
+    --yellow: #eab900;
+  }
+
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body,
+  html {
+    font-size: 75%;
+    font-family: 'Space Mono', monospace;
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyles;
