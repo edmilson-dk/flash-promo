0784dfecb2b857afbcc86e9d5e94bef3ee6060df
edmilson-dk <droidvidaboa@gmail.com>
2021-03-24 15:04:26 -0300

    feat: created button start component

---
+++ src/styles/components/ButtonStart/index.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const ButtonStartWrapper = styled.button`
+  width: 100%;
+  max-width: 230px;
+  padding: 0.833rem 1rem;
+  background-color: var(--green);
+  border-radius: 10px;
+
+  > a {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  }
+
+  > a strong {
+    font-size: 1.5rem;
+    font-weight: 600;
+    text-align: left;
+    color: var(--white);
+  }
+
+  > a span {
+    width: 2.5rem;
+    height: 2.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    > svg {
+      stroke: var(--white);
+    }
+  }
+`;
