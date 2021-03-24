76c95621f55f3b863d18218a8a070c4e7df14332
edmilson-dk <droidvidaboa@gmail.com>
2021-03-24 15:00:27 -0300

    feat: created set time box component

--- src/components/SetTimeBox/index.tsx
+++ src/components/SetTimeBox/index.tsx
@@ -5,29 +5,34 @@ import ControlSetTimeButton from "../ControlSetTimeButton";
 import {
   SetTimeBoxWrapper,
   SetTimeBoxButtons,
-  SetTimeBoxValue
+  SetTimeBoxValue,
+  SetTimeBoxTitle
 } from "src/styles/components/SetTimeBox";
 
 type SetTimeBoxProps = {
   value: number;
+  title: string;
   setValueUp: () => void;
   setValueDown: () => void;
 }
 
-function SetTimeBox({ value, setValueUp, setValueDown }: SetTimeBoxProps) {
+function SetTimeBox({ value, title, setValueUp, setValueDown }: SetTimeBoxProps) {
   return (
     <SetTimeBoxWrapper>
-      <SetTimeBoxButtons>
-        <ControlSetTimeButton 
-          icon={<FiArrowUp size="100%"/>} 
-          onClick={setValueUp}/>
-        <ControlSetTimeButton 
-          icon={<FiArrowDown size="100%"/>} 
-          onClick={setValueDown}/>
-      </SetTimeBoxButtons>
-      <SetTimeBoxValue>
-        <strong>{ value }</strong>
-      </SetTimeBoxValue>
+      <SetTimeBoxTitle>{ title }</SetTimeBoxTitle>
+      <article>
+        <SetTimeBoxButtons>
+          <ControlSetTimeButton 
+            icon={<FiArrowUp size="100%"/>} 
+            onClick={setValueUp}/>
+          <ControlSetTimeButton 
+            icon={<FiArrowDown size="100%"/>} 
+            onClick={setValueDown}/>
+        </SetTimeBoxButtons>
+        <SetTimeBoxValue>
+          <strong>{ value }</strong>
+        </SetTimeBoxValue>
+      </article>
     </SetTimeBoxWrapper>
   )
 }
