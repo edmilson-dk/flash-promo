5489be6b5d534bb9aa74b77912c30cd0c17e2cd7
edmilson-dk <droidvidaboa@gmail.com>
2021-03-24 22:41:14 -0300

    feat: created value box rouded component styles

---
+++ src/styles/components/ValueBoxRouded/index.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const ValueBoxRoudedWrapper = styled.article`
+  width: 260px;
+  height: 260px;
+  border-radius: 50%;
+  background-color: var(--dark-light);
+  box-shadow: 0 0 3px 3px rgba(244, 244, 244, 0.2);
+  padding: 1.25rem;
+`;
+
+export const ValueBoxRoudedContainer = styled.article`
+  width: 100%;
+  height: 100%;
+
+  border-radius: 50%;
+  border: 5px solid var(--green);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  > div {
+    > strong {
+      font-size: 5rem;
+      font-weight: 700;
+      color: var(--white);
+      text-align: center;
+    }
+  }
+
+  > span {
+      font-size: 5rem;
+      color: var(--white);
+      text-align: center;
+      font-weight: 600;
+    }
+`;
