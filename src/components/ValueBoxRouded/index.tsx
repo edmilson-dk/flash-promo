094a87ce86f6f38d8a04674b8f85af25a749f0c1
edmilson-dk <droidvidaboa@gmail.com>
2021-03-26 13:06:45 -0300

    feat: added times values components

--- src/components/ValueBoxRouded/index.tsx
+++ src/components/ValueBoxRouded/index.tsx
@@ -1,17 +1,29 @@
 import { ValueBoxRoudedWrapper, ValueBoxRoudedContainer } from "src/styles/components/ValueBoxRouded";
 
-function ValueBoxRouded() {
+type ValueBoxRoudedProps = {
+  minuteLeft: number;
+  minuteRight: number;
+  secondLeft: number;
+  secondRight: number;
+}
+
+function ValueBoxRouded({ 
+  minuteLeft,
+  minuteRight,
+  secondLeft,
+  secondRight
+}: ValueBoxRoudedProps) {
   return (
     <ValueBoxRoudedWrapper>
       <ValueBoxRoudedContainer>
         <div>
-          <strong>2</strong>
-          <strong>5</strong>
+          <strong>{ minuteLeft }</strong>
+          <strong>{ minuteRight }</strong>
         </div>
           <span>:</span>
         <div>
-          <strong>0</strong>
-          <strong>0</strong>
+          <strong>{ secondLeft }</strong>
+          <strong>{ secondRight }</strong>
         </div>
       </ValueBoxRoudedContainer>
     </ValueBoxRoudedWrapper>
