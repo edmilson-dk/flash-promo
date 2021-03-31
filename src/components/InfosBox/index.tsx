07100eed50906601843503cab50aebfb18ae4645
edmilson-dk <droidvidaboa@gmail.com>
2021-03-31 13:45:47 -0300

    feat: added news props on infos box component

--- src/components/InfosBox/index.tsx
+++ src/components/InfosBox/index.tsx
@@ -10,29 +10,43 @@ import {
 
 type InfosBoxProps = {
   title: string;
+  isPause: boolean;
+  ciclesCount: number;
+  ciclesFinishedCount: number;
   onClickPlay: () => void;
 }
 
-function InfosBox({ title , onClickPlay }: InfosBoxProps) {
+function InfosBox({ 
+  title , 
+  onClickPlay, 
+  isPause,
+  ciclesCount,
+  ciclesFinishedCount,
+}: InfosBoxProps) {
+  function generateDost(count: number) {
+    const dots = [];
+
+    for (let index = 0; index < count; index++) {
+      dots.push([<span></span>]);
+    }
+
+    return dots;
+  }
+
   return (
     <InfosBoxWrapper>
       <InfosBoxContainer>
-        <InfosBoxTitle>{ title }</InfosBoxTitle>
-        <InfosBoxDots>
-          <span></span>
-          <span></span>
-          <span></span>
+        <InfosBoxTitle isPause={isPause}>{ title }</InfosBoxTitle>
+        <InfosBoxDots countActive={ciclesFinishedCount}>
+          {
+            generateDost(ciclesCount).map(item => (item))
+          }
         </InfosBoxDots>
         <InfosBoxButtonsWrapper>
           <button onClick={onClickPlay}>
               <span>
                 <FiPlay size="100%"/>
               </span>
-            </button>
-          <button>
-            <span>
-              <FiRepeat size="100%"/>
-            </span>
           </button>
         </InfosBoxButtonsWrapper>
       </InfosBoxContainer>
